a3e1a7b8d0a3ea5f23e9b34cc9ad3077c48cc92b
117503445 <t117503445@gmail.com>
2019-01-06 23:02:55 +0800

    Update 66e.c

--- 66e.c
+++ 66e.c
@@ -17,61 +17,100 @@ void BubbleSort(int a[], int n, int b);
 int main()
 {
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 	int a[n][n];
-	int i,j;
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			scanf("%d",&a[i][j]);
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			scanf("%d", &a[i][j]);
 		}
 	}
-	
-	int z=1;//0-leftdown,1-rightup
-	int x=0,y=0;
-	printf("%d ",a[x][y]);
-	i=0;
-	while(1){
-		i++;if(i>50){break;
-		}
-		
-		if(z==0){
-			if(y-1<0){
-				z=1;
-				if(x+1<n){
-					x++;
-				}else{
-					y++;
+
+	int z = 1; //0-leftdown,1-rightup
+	int x = 0, y = 0;
+	printf("%d ", a[x][y]);
+	i = 0;
+	if (n != 1)
+	{
+		while (1)
+		{
+			i++;
+			if (i > 50)
+			{
+				break;
+			}
+
+			if (z == 0)
+			{
+				if (y - 1 < 0)
+				{
+					z = 1;
+					if (x + 1 < n)
+					{
+						x++;
+					}
+					else
+					{
+						y++;
+					}
 				}
-			}else{				
-				if(x+1<n){
-				x++;y--;}else{
-					y++;z=1;
+				else
+				{
+					if (x + 1 < n)
+					{
+						x++;
+						y--;
+					}
+					else
+					{
+						y++;
+						z = 1;
+					}
 				}
 			}
-		}else{
-			if(x-1<0){
-				z=0;
-				if(y+1<n){
-					y++;
-				}else{
-					x++;
+			else
+			{
+				if (x - 1 < 0)
+				{
+					z = 0;
+					if (y + 1 < n)
+					{
+						y++;
+					}
+					else
+					{
+						x++;
+					}
 				}
-			}else{
-					if(y+1<n){
-				x--;y++;}else{
-					x++;z=0;
+				else
+				{
+					if (y + 1 < n)
+					{
+						x--;
+						y++;
+					}
+					else
+					{
+						x++;
+						z = 0;
+					}
 				}
 			}
-		}
-		//printf("\nx=%d,y=%d,z=%d\n",x,y,z);
-		printf("%d ",a[x][y]);
-		if(x==n-1&&y==n-1){
-			break;
+			//printf("\nx=%d,y=%d,z=%d\n",x,y,z);
+			printf("%d ", a[x][y]);
+			if (x == n - 1 && y == n - 1)
+			{
+				break;
+			}
 		}
 	}
-	
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
 			//printf("%d",a[i][j]);
 		}
 	}
